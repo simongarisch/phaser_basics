603a423317c6ec40c40828f15bc415de90947454
simongarisch <gatman946@gmail.com>
2018-12-23 08:53:41 +1100

    Adding some more examples

---
+++ grid_align/grid_align.js
@@ -0,0 +1,37 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    // see the assets and examples here: https://github.com/photonstorm/phaser3-examples
+    this.load.spritesheet('diamonds', 'diamonds32x24x5.png', { frameWidth: 32, frameHeight: 24 });
+}
+
+function create ()
+{
+    var group = this.add.group({
+        key: 'diamonds',
+        frame: [ 0, 1, 2, 3, 4 ],
+        frameQuantity: 20
+    });
+
+    Phaser.Actions.GridAlign(group.getChildren(), {
+        width: 10,
+        height: 10,
+        cellWidth: 32,
+        cellHeight: 32,
+        x: 100,
+        y: 100
+    });
+}
